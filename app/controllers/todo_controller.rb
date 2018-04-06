31e430db193e9a0c47bc2b8f1164cdd459e017f9
beatriz mata <bmata1@cps.edu>
2018-04-06 18:52:48 +0000

    Define instance variables in Todo show action, and place in show view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,13 @@
 class TodoController < ApplicationController
     def index
   	end
+  	
+  	def show
+end
+
+@todo_description = "Learn Variables"
+@todo_pomodoro_estimate = 4
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
  
  get 'todo/index', to: 'todo#index'
  
+ get 'todo/show', to: 'todo#show'
+ 
 end
